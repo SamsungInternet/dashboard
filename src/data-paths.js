e5c22d0fb65e7367844e0bd5f10db0cd06fef6b3
Peter O'Shaughnessy <peter.oshaughnessy@gmail.com>
2017-06-19 15:59:03 +0100

    Add file missing from previous check-in

---
+++ src/data-paths.js
@@ -0,0 +1,3 @@
+/* Exporting as variables to be used by both front-end and service worker */
+var mediumStatsCSVPath = 'data/medium-stats-overview-2017-06-14.csv';
+var statsJSONPath = 'data/stats-2017-06-14.json';
