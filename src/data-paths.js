46a6609f14f4c4b8b1b51dddd2d32786b2be9aff
Peter O'Shaughnessy <peter.oshaughnessy@gmail.com>
2017-08-07 13:05:48 +0100

    Update Medium graph

--- src/data-paths.js
+++ src/data-paths.js
@@ -1,4 +1,4 @@
 /* Exporting as variables to be used by both front-end and service worker */
-var mediumStatsCSVPath = 'data/medium-stats-overview-2017-08-01.csv';
+var mediumStatsCSVPath = 'data/medium-stats-overview-2017-08-07.csv';
 var statsJSONPath = 'data/stats-2017-08-07.json';
 var compareWithStatsJSONPath = 'data/stats-2017-07-11.json';
