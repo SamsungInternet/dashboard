c76fb0d001bbcb8366cb52d424e7c6c036573344
Peter O'Shaughnessy <peter.oshaughnessy@gmail.com>
2017-10-24 10:14:14 +0100

    Count 2017 Medium views

--- extension/content-script-medium-stories.js
+++ extension/content-script-medium-stories.js
@@ -5,21 +5,33 @@
 var viewsCount = 0;
 var statsTableBody = document.getElementsByClassName('js-statsTableBody')[0];
 var trs = statsTableBody.getElementsByTagName('tr');
+var printed2017Total = false;
 
 for (var i=0; i < trs.length; i++) {
   
   var tr = trs[i];
-  
+
   if (tr.children.length < 2) {
+
+    // Check date heading
+    if (!printed2017Total && tr.classList.contains('sortableTable-row--dateBucket')) {
+      var dateHeading = tr.querySelector('.heading-title');
+      if (dateHeading && dateHeading.innerHTML.indexOf('2017') < 0) {
+        console.log('* 2017 total', viewsCount);
+        printed2017Total = true;
+        continue;
+      }
+    } 
+
     continue;
   }
-  
+
   var viewsTd = tr.children[1];
   var viewsValue = viewsTd.getElementsByClassName('sortableTable-value')[0].innerText;
   
-  console.log('views', viewsValue);
+  console.log('Views', viewsValue);
   
   viewsCount += parseInt(viewsValue, 10);
 }
 
-console.log('Total views count', viewsCount);
+console.log('* Total views count', viewsCount);
