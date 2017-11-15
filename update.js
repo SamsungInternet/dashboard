1224a0c935d0ad64e3142d7170e3997f8ee8d9cd
Peter O'Shaughnessy <peter.oshaughnessy@gmail.com>
2017-11-15 13:30:54 +0000

    Added Stack Overflow stats

--- update.js
+++ update.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const dotenv = require('dotenv');
+const assert = require('assert');
 const template = require('es6-template-strings');
 const fetch = require('node-fetch');
 const httpsProxyAgent = require('https-proxy-agent');
@@ -6,6 +8,17 @@ const moment = require('moment');
 const utils = require('./utils');
 const stats = require('./data/general/2017-11-13-stats.json');
 const comparisonStats = require('./data/general/2017-10-10-stats.json');
+
+// Load local environment variables from .env
+dotenv.load({silent: true});
+
+assert(process.env.STACK_OVERFLOW_API_KEY, 'missing STACK_OVERFLOW_API_KEY in env');
+
+const STACK_OVERFLOW_FROM_DATE_SECS = Math.floor(moment(comparisonStats.updated).valueOf() / 1000);
+const STACK_OVERFLOW_KEY = process.env.STACK_OVERFLOW_API_KEY;
+const STACK_OVERFLOW_USER_IDS = [396246,4007679,2144525];
+const STACK_OVERFLOW_QUESTIONS_URL = `https://api.stackexchange.com/2.2/search/advanced?site=stackoverflow&order=desc&sort=creation&key=${STACK_OVERFLOW_KEY}&q=samsung%20internet&fromdate=${STACK_OVERFLOW_FROM_DATE_SECS}`; 
+const STACK_OVERFLOW_ANSWERS_URL = `https://api.stackexchange.com/2.2/users/${STACK_OVERFLOW_USER_IDS.join(';')}/answers?site=stackoverflow&order=desc&sort=activity&key=${STACK_OVERFLOW_KEY}&fromdate=${STACK_OVERFLOW_FROM_DATE_SECS}`;
 
 const GITHUB_API_REPOS_URL = 'https://api.github.com/search/repositories?q=org%3Asamsunginternet';
 
@@ -15,6 +28,13 @@ const noChangeArrow = '‒';
 
 const comparisonDaysDiff = utils.getDaysDiff(stats.updated, comparisonStats.updated);
 
+const proxy = process.env.http_proxy;
+let fetchOptions = null;
+
+if (proxy) {
+    console.log('Using proxy server', proxy);
+    fetchOptions = {agent: new httpsProxyAgent(proxy)};
+}
 
 function updateStatWithChange(data, comparisonData, pathToStat, lowerIsBetter) {
 
@@ -29,7 +49,6 @@ function updateStatWithChange(data, comparisonData, pathToStat, lowerIsBetter) {
 
     stat.formattedCount = utils.formatNumberValue(count);
     stat.change = utils.formatChangeValue(count, comparisonCount, lowerIsBetter);
-    stat.changeLabel = comparisonDaysDiff + ' days';
     stat.changeDirection = changeDirection;
 
     if (changeDirection === 'up') {
@@ -75,6 +94,7 @@ function processStats(stats, comparisonStats) {
 
   // Add some additional data we want to display
   processedStats.lastUpdated = moment(stats.updated).format('DD MMMM YYYY');
+  processedStats.changeSinceLabel = comparisonDaysDiff + ' days';
   
   processedStats.totalFollowers = utils.formatNumberValue(
         stats.medium.audience.followers.count +
@@ -88,6 +108,57 @@ function processStats(stats, comparisonStats) {
         stats.medium.engagement.views.count);
 
   return processedStats;
+
+}
+
+async function fetchGithubStats() {
+
+    console.log('Fetching Github stats...');
+
+    try {
+        const response = await fetch(GITHUB_API_REPOS_URL, fetchOptions);
+        return await response.json();  
+    } catch(error) {
+        console.log('Error fetching Github stats', error);
+    };
+
+}
+
+async function fetchStackOverflowQuestionStats() {
+
+    console.log('Fetching Stack Overflow question stats');
+
+    try {
+        const response = await fetch(STACK_OVERFLOW_QUESTIONS_URL, fetchOptions);
+        return await response.json();
+    } catch(error) {
+        console.log('Error fetching Stack Overflow question stats', error);
+    }
+
+} 
+
+async function fetchStackOverflowAnswerStats() {
+
+    console.log('Fetching Stack Overflow answer stats');
+
+    try {
+        const response = await fetch(STACK_OVERFLOW_ANSWERS_URL, fetchOptions);
+        return await response.json();
+    } catch(error) {
+        console.log('Error fetching Stack Overflow question stats', error);
+    }
+
+}
+
+async function fetchStackOverflowStats() {
+
+    try {
+        const questionStats = await fetchStackOverflowQuestionStats();
+        const answerStats = await fetchStackOverflowAnswerStats();
+        return {questions: questionStats, answers: answerStats};
+    } catch(error) {
+        console.log('Error fetching Stack Overflow stats', error);
+    }
 
 }
 
@@ -118,37 +189,41 @@ function updateWithGithubStats(processedStats, githubStats) {
 
 }
 
-const templateHtml = fs.readFileSync('src/template.html', 'utf8');
-let processedStats = processStats(stats, comparisonStats);
+function updateWithStackOverflowStats(processedStats, stackOverflowStats) {
 
-const proxy = process.env.http_proxy;
-let options = null;
+    processedStats.stackoverflow = {
+        questions: stackOverflowStats.questions.items.length,
+        answers: stackOverflowStats.answers.items.length
+    };
 
-if (proxy) {
-    console.log('Using proxy server', proxy);
-    options = {agent: new httpsProxyAgent(proxy)};
 }
 
-console.log('Fetching Github stats...');
+function writeHtml(processedStats) {
 
-fetch(GITHUB_API_REPOS_URL, options)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(githubStats) {
-  
-      updateWithGithubStats(processedStats, githubStats);
+    const templateHtml = fs.readFileSync('src/template.html', 'utf8');    
+    const html = template(templateHtml, processedStats);
 
-      const html = template(templateHtml, processedStats);
-
-      fs.writeFile('index.html', html, function(err) {
+    fs.writeFile('index.html', html, function(err) {
         if (err) {
-          console.log('Error writing index.html', err);
+        console.log('Error writing index.html', err);
         } else {
-          console.log('Updated index.html ✔');
+        console.log('Updated index.html ✔');
         }
-      });
-  })
-  .catch(function(error) {
-      console.log('Error fetching Github stats', error);
-  });
+    });
+}
+
+async function fetchStatsAndWriteHtml() {
+
+    let processedStats = processStats(stats, comparisonStats);
+
+    const githubStats = await fetchGithubStats();
+    updateWithGithubStats(processedStats, githubStats);
+
+    const stackOverflowStats = await fetchStackOverflowStats();
+    updateWithStackOverflowStats(processedStats, stackOverflowStats);
+
+    writeHtml(processedStats);
+
+}
+
+fetchStatsAndWriteHtml();
