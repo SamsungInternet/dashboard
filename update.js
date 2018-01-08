c265a6f5f2ee9b8bf104057825ff9288f9f73340
Peter O'Shaughnessy <peter.oshaughnessy@gmail.com>
2018-01-08 09:45:01 +0000

    Update stats

--- update.js
+++ update.js
@@ -10,8 +10,8 @@ const utils = require('./utils');
 /**
  * Update these appropriately each time. Also see: `src/data-paths.js`.
  */
-const stats = require('./data/general/2017-12-12-stats.json');
-const comparisonStats = require('./data/general/2017-11-13-stats.json');
+const stats = require('./data/general/2018-01-08-stats.json');
+const comparisonStats = require('./data/general/2017-12-12-stats.json');
 
 // Load local environment variables from .env
 dotenv.load({silent: true});
