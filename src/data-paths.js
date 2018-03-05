ff3bb685f96e44a7cd3070ab5d8b040b42cbc4ab
Peter O'Shaughnessy <peter.oshaughnessy@gmail.com>
2018-03-05 18:56:08 +0000

    Update dashboard stats

--- src/data-paths.js
+++ src/data-paths.js
@@ -1,2 +1,2 @@
 /* Exporting as variables to be used by both front-end and service worker */
-var mediumStatsCSVPath = 'data/medium/2018-02-26-medium-overview.csv';
+var mediumStatsCSVPath = 'data/medium/2018-03-05-medium-overview.csv';
