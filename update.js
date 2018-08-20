13773875e546b4fb75a958ee5873aeb7b94eddb8
Peter Oshaughnessy <peter.oshaughnessy@gmail.com>
2018-08-20 13:20:33 +0100

    Update stats

--- update.js
+++ update.js
@@ -10,8 +10,8 @@ const utils = require('./utils');
 /**
  * Update these appropriately each time. Also see: `src/data-paths.js`.
  */
-const stats = require('./data/general/2018-08-07-stats.json');
-const comparisonStats = require('./data/general/2018-07-10-stats.json');
+const stats = require('./data/general/2018-08-20-stats.json');
+const comparisonStats = require('./data/general/2018-07-24-stats.json');
 const GITHUB_PULL_REQUESTS_SINCE_DATE = '2018-07-01';
 
 // Load local environment variables from .env
